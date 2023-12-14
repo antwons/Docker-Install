f8d78a72ce144557fa3bc7f44dbe940730479243
Anthony <117550167+antwons@users.noreply.github.com>
2023-12-13 21:10:23 -0800

    Update debian-build.sh

--- scripts/debian-build.sh
+++ scripts/debian-build.sh
@@ -41,7 +41,7 @@ clear
 
 # Add Docker's official GPG key:
 apt-get update
-apt-get install ca-certificates curl gnupg
+apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
