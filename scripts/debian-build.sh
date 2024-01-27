a72bddf1201fe6eb20a0cc5605a7aeebd58ff7dd
Anthony <117550167+antwons@users.noreply.github.com>
2024-01-26 23:37:49 -0800

    Update debian-build.sh

--- scripts/debian-build.sh
+++ scripts/debian-build.sh
@@ -40,12 +40,12 @@ sleep 2
 clear
 
 apt-get update
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
 apt-cache policy docker-ce
-apt install docker-ce
+apt install -y docker-ce
 clear
 sleep 2
 
